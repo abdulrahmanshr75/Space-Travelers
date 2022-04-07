81ee6cf38308010c104f082962376e5886e5fae3
ayubch <ayoub.chahir96@gmail.com>
2022-04-07 15:39:41 +0000

    Add Pages folder

---
+++ src/Pages/Header.js
@@ -0,0 +1,8 @@
+const Header = () => (
+  <div className="headerWrap">
+    <img src="../planet.png" alt="haha" className="logoImg" />
+    <h1>Space Traveler&apos;s</h1>
+  </div>
+);
+
+export default Header;
---
+++ src/Pages/NavBar.js
@@ -0,0 +1,45 @@
+import { NavLink } from 'react-router-dom';
+import Header from './Header';
+
+const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/Missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      path: '/My Profile',
+      text: 'My Profile',
+    },
+  ];
+  return (
+    <nav className="navBar">
+      <div className="navWrap">
+        <Header />
+        <ul className="navLinks">
+          {links.map((link) => (
+            <li key={link.id} className="navLink">
+              <NavLink
+                to={link.path}
+                exact={true}
+                data-testid={link.id}
+                activeClassName="active"
+              >
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
